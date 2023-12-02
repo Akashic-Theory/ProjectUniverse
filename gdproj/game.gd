080a95925f51f68a81701e4512c0f377cc1a031c
Patrick Breen <breenboyz@gmail.com>
2023-12-01 18:56:16 -0600

    :lipstick: Added settings menu
    	- Controls can be modified by pressing Escape in game
    	- These controls will be saved to file when you close the menu

---
+++ gdproj/game.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var settings: GameSettings
+
+func _ready():
+	# Load settings from file
+	settings = load("res://saved_settings.tres") as GameSettings
+	
+	# If no settings file exists, create a new one
+	if settings == null:
+		settings = GameSettings.new()
+		save_settings()
+	else:
+		settings.bind_controls_from_file()
+	
+
+func save_settings():
+	ResourceSaver.save(settings, "res://saved_settings.tres")
