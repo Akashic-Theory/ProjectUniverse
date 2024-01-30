69a8c7438f29de586d3203f934b877d63fe335b4
Patrick Breen <breenboyz@gmail.com>
2024-01-29 20:16:27 -0600

    :sparkles: UI button images now load dynamically
    	- Images for UI are now stored as small sprite sheets

--- gdproj/UI/player_action_buttons.gd
+++ gdproj/UI/player_action_buttons.gd
@@ -3,9 +3,12 @@ extends Container
 var character: Character
 var buttons: Array
 
+@export var sprite_sets: Array[SpriteSet]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	buttons = $HBoxContainer.get_children()
+	sprite_sets[0].apply_to(buttons[0])
 	
 	# Connect signal to allow movement label to update
 	character.moved.connect($HBoxContainer/MoveButton/RemainingMoveLabel.update_label)
