cfbb29a27f65a8438e32a9a0ff33a0d0a09aa4a9
Patrick Breen <breenboyz@gmail.com>
2024-04-02 20:00:20 -0500

    :zap: Improved mesh drawing for lines

--- extension/include/util/GraphicalUtility.h
+++ extension/include/util/GraphicalUtility.h
@@ -5,16 +5,20 @@
 
 #include "godot_cpp/classes/node3d.hpp"
 #include "godot_cpp/classes/mesh_instance3d.hpp"
+#include "godot_cpp/classes/orm_material3d.hpp"
 
 class GraphicalUtility: public godot::Node3D {
     GDCLASS(GraphicalUtility, godot::Node3D);
 
 private:
-    static std::vector<godot::MeshInstance3D*> storedMeshes;
+//    static std::vector<godot::MeshInstance3D*> storedMeshes;
+    static godot::ORMMaterial3D* pathMaterial;
 protected:
     static void _bind_methods();
 public:
+    static void initialize_resources();
     static godot::MeshInstance3D* get_path_mesh(const godot::PackedVector3Array& path);
+    static bool update_path_mesh(godot::MeshInstance3D* meshInstance, const godot::PackedVector3Array& path);
     static godot::MeshInstance3D* get_point_mesh(const godot::Vector3& pos);
 };
 
--- extension/src/util/GraphicalUtility.cpp
+++ extension/src/util/GraphicalUtility.cpp
@@ -7,18 +7,27 @@
 #include "godot_cpp/classes/sphere_mesh.hpp"
 #include "godot_cpp/classes/orm_material3d.hpp"
 #include "godot_cpp/classes/scene_tree.hpp"
+#include <godot_cpp/variant/utility_functions.hpp>
 
 #include "util/GraphicalUtility.h"
+
+godot::ORMMaterial3D* GraphicalUtility::pathMaterial;
+
+void GraphicalUtility::initialize_resources() {
+    pathMaterial = memnew(godot::ORMMaterial3D);
+    pathMaterial->set_shading_mode(godot::BaseMaterial3D::SHADING_MODE_UNSHADED);
+    pathMaterial->set_albedo(godot::Color::named("Aquamarine"));
+}
 
 godot::MeshInstance3D* GraphicalUtility::get_path_mesh(const godot::PackedVector3Array& path) {
     godot::MeshInstance3D* meshInstance = memnew(godot::MeshInstance3D);
     godot::ImmediateMesh* immediateMesh = memnew(godot::ImmediateMesh);
-    godot::ORMMaterial3D* material = memnew(godot::ORMMaterial3D);
+//    godot::ORMMaterial3D* material = memnew(godot::ORMMaterial3D);
 
     meshInstance->set_mesh(immediateMesh);
     meshInstance->set_cast_shadows_setting(godot::GeometryInstance3D::SHADOW_CASTING_SETTING_OFF);
 
-    immediateMesh->surface_begin(godot::Mesh::PRIMITIVE_LINE_STRIP, material);
+    immediateMesh->surface_begin(godot::Mesh::PRIMITIVE_LINE_STRIP, pathMaterial);
 
     for (auto& pos: path) {
         immediateMesh->surface_add_vertex(pos);
@@ -26,10 +35,31 @@ godot::MeshInstance3D* GraphicalUtility::get_path_mesh(const godot::PackedVector
 
     immediateMesh->surface_end();
 
-    material->set_shading_mode(godot::BaseMaterial3D::SHADING_MODE_UNSHADED);
-    material->set_albedo(godot::Color::named("Aquamarine"));
+//    material->set_shading_mode(godot::BaseMaterial3D::SHADING_MODE_UNSHADED);
+//    material->set_albedo(godot::Color::named("Aquamarine"));
 
     return meshInstance;
+}
+
+bool GraphicalUtility::update_path_mesh(godot::MeshInstance3D* meshInstance, const godot::PackedVector3Array& path) {
+    if (!meshInstance || path.size() < 2) {
+        return false;
+    }
+    godot::Vector3 offset = path[0];
+
+    godot::ImmediateMesh* immediateMesh = (godot::ImmediateMesh*) (*meshInstance->get_mesh());
+//    godot::Material* material = *immediateMesh->_surface_get_material(0);
+
+    immediateMesh->clear_surfaces();
+    immediateMesh->surface_begin(godot::Mesh::PRIMITIVE_LINE_STRIP, pathMaterial);
+
+    for (auto& pos: path) {
+        immediateMesh->surface_add_vertex(pos - offset);
+    }
+
+    immediateMesh->surface_end();
+
+    return true;
 }
 
 godot::MeshInstance3D* GraphicalUtility::get_point_mesh(const godot::Vector3 &pos) {
@@ -53,9 +83,15 @@ godot::MeshInstance3D* GraphicalUtility::get_point_mesh(const godot::Vector3 &po
 
 void GraphicalUtility::_bind_methods() {
     using namespace godot;
+
+    initialize_resources();
+
     ClassDB::bind_static_method(GraphicalUtility::get_class_static(),
                                 D_METHOD("path_mesh", "path"),
                                 &GraphicalUtility::get_path_mesh);
+    ClassDB::bind_static_method(GraphicalUtility::get_class_static(),
+                                D_METHOD("update_path_mesh", "meshInstance", "path"),
+                                &GraphicalUtility::update_path_mesh);
     ClassDB::bind_static_method(GraphicalUtility::get_class_static(),
                                 D_METHOD("point_mesh", "pos"),
                                 &GraphicalUtility::get_point_mesh);
