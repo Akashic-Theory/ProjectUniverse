0e7c391cc5590f8801ee7373e08833a2298b4dc1
Patrick Breen <breenboyz@gmail.com>
2024-04-29 14:03:30 -0500

    :sparkles: Settings screen rework
    	- Added fullscreen toggle

--- gdproj/UI/settings_menu.gd
+++ gdproj/UI/settings_menu.gd
@@ -1,24 +1,22 @@
 extends Control
 
+var control_prefab = preload("res://UI/control_setting.tscn")
+
 @onready var is_assigning = false
+var queued_graphics_changes = { }
 
 @onready var graphics_button_list = {
-	"fullscreen": find_child("AssignFullscreen") as BaseButton
+	"windowMode": find_child("AssignFullscreen") as BaseButton
 }
 
-@onready var controls_button_list = {
-	"move_hover0": find_child("AssignMove0") as Button,
-	"move_hover1": find_child("AssignMove1") as Button,
-	"end_turn0": find_child("AssignEndTurn0") as Button,
-	"end_turn1": find_child("AssignEndTurn1") as Button,
-	"use_ability_10": find_child("AssignAbilityOne0") as Button,
-	"use_ability_11": find_child("AssignAbilityOne1") as Button,
-	"use_ability_20": find_child("AssignAbilityTwo0") as Button,
-	"use_ability_21": find_child("AssignAbilityTwo1") as Button,
-	"use_ability_30": find_child("AssignAbilityThree0") as Button,
-	"use_ability_31": find_child("AssignAbilityThree1") as Button,
-	"use_ability_40": find_child("AssignAbilityFour0") as Button,
-	"use_ability_41": find_child("AssignAbilityFour1") as Button
+@onready var controls_button_list = { }
+@onready var readable_control_names = {
+	"move_hover": "Move",
+	"end_turn": "End Turn",
+	"use_ability_1": "Ability 1",
+	"use_ability_2": "Ability 2",
+	"use_ability_3": "Ability 3",
+	"use_ability_4": "Ability 4"
 }
 
 # Globals for keybinding
@@ -27,28 +25,71 @@ var cur_bind_index: int
 
 
 func _ready():
-	# graphics inputs will have varied logics, so they cannot be iterated
-	graphics_button_list["fullscreen"].button_pressed = Game.settings.graphics["fullscreen"]
+	visibility_changed.connect(_visibility_changed)
 	
-	for button in controls_button_list:
-		var index = button[-1] as int
-		var events = InputMap.action_get_events(button.substr(0, button.length() - 1))
-		if events.size() <= index:
-			push_warning("Button ", button, " does not exist for InputMap")
-			continue
-		# Sets the default text of each button to be whatever is initially stored as the keybindings
-		# Note: tried using .keycode, returns 0 when not set by game settings file. .physical_keycode works, though. Bug?
-		controls_button_list[button].set_text(OS.get_keycode_string(InputMap.action_get_events(button.substr(0, button.length() - 1))[button[-1] as int].physical_keycode))
+	# graphics inputs will have varied logics, so they cannot be iterated
+	graphics_button_list["windowMode"].add_item("Windowed", DisplayServer.WINDOW_MODE_MAXIMIZED)
+	graphics_button_list["windowMode"].add_item("Borderless", DisplayServer.WINDOW_MODE_FULLSCREEN)
+	graphics_button_list["windowMode"].add_item("Fullscreen", DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+	graphics_button_list["windowMode"].selected = Game.settings.graphics["windowMode"]
+	
+	for control in Game.settings.controls:
+		var ui = control_prefab.instantiate()
+		
+		# If this control is mapped as a readable name, use that
+		if control in readable_control_names:
+			ui.find_child("Label").set_text(readable_control_names[control])
+		else:
+			ui.find_child("Label").set_text(control)
+		
+		var primary = ui.find_child("Primary") as Button
+		primary.pressed.connect(_start_assign_key.bind(control, 0))
+		if InputMap.action_get_events(control).size() >= 1:
+			primary.set_text(OS.get_keycode_string(InputMap.action_get_events(control)[0].physical_keycode))
+		controls_button_list["%s_primary" % control] = primary
+		
+		var secondary = ui.find_child("Secondary") as Button
+		secondary.pressed.connect(_start_assign_key.bind(control, 1))
+		if InputMap.action_get_events(control).size() >= 2:
+			secondary.set_text(OS.get_keycode_string(InputMap.action_get_events(control)[1].physical_keycode))
+		controls_button_list["%s_secondary" % control] = secondary
+		
+		%ControlsBox.add_child(ui)
+		pass
+	
+	#for button in controls_button_list:
+		#var index = button[-1] as int
+		#var events = InputMap.action_get_events(button.substr(0, button.length() - 1))
+		#if events.size() <= index:
+			#push_warning("Button ", button, " does not exist for InputMap")
+			#continue
+		## Sets the default text of each button to be whatever is initially stored as the keybindings
+		## Note: tried using .keycode, returns 0 when not set by game settings file. .physical_keycode works, though. Bug?
+		#controls_button_list[button].set_text(OS.get_keycode_string(InputMap.action_get_events(button.substr(0, button.length() - 1))[button[-1] as int].physical_keycode))
 
 
-func _toggle_graphics_setting(val: bool, tag: String):
-	Game.settings.toggle_graphics_setting(tag, val)
+# Reset UI features for non-applied changes
+func _visibility_changed():
+	print(Game.settings.graphics["windowMode"])
+	graphics_button_list["windowMode"].selected = Game.settings.graphics["windowMode"]
+
+
+func _toggle_graphics_setting(val: Variant, tag: String):
+	queued_graphics_changes[tag] = val
+	#Game.settings.toggle_graphics_setting(tag, val)
 
 
 func _start_assign_key(key: String, bind_index: int):
 	cur_action = key
 	cur_bind_index = bind_index
 	is_assigning = true
+
+
+func _apply_settings():
+	for setting in queued_graphics_changes:
+		Game.settings.toggle_graphics_setting(setting, queued_graphics_changes[setting])
+	
+	Game.settings.apply_graphics_from_saved()
 
 
 # Key reassignment
@@ -61,24 +102,25 @@ func _input(event):
 	get_viewport().set_input_as_handled()
 	
 	if event is InputEventKey:
-		var event_list = InputMap.action_get_events(cur_action)
-		var event_is_duplicate = event_list.any(func(e): return e.keycode == event.keycode)
+		var event_list = Game.settings.controls[cur_action]
+		var event_is_duplicate = event_list.any(func(e): return e != null && e.keycode == event.keycode)
+		print(event_is_duplicate)
 		
 		# If the action doesn't have this index yet,
 		# then there is no existing control to rebind
-		if event_list.size() <= cur_bind_index:
+		if null in event_list:
 			if !event_is_duplicate:
-				Game.settings.bind_control(cur_action, event)
+				Game.settings.rebind_control(cur_action, null, event)
 		else:
 			# If the event is already assigned to this action,
 			# then just remove the event currently in place
 			if event_is_duplicate && event_list[cur_bind_index].keycode != event.keycode:
-				Game.settings.unbind_control(cur_action, event_list[cur_bind_index])
-				controls_button_list[cur_action + str(cur_bind_index)].set_text("")
+				Game.settings.rebind_control(cur_action, event_list[cur_bind_index], null)
+				controls_button_list[cur_action + ("_primary" if cur_bind_index == 0 else "_secondary")].set_text("")
 			else:
 				Game.settings.rebind_control(cur_action, event_list[cur_bind_index], event)
 		
 		# Update the button text
 		if !event_is_duplicate:
-			controls_button_list[cur_action + str(cur_bind_index)].set_text(OS.get_keycode_string(event.keycode))
+			controls_button_list[cur_action + ("_primary" if cur_bind_index == 0 else "_secondary")].set_text(OS.get_keycode_string(event.keycode))
 		is_assigning = false
--- gdproj/game_settings.gd
+++ gdproj/game_settings.gd
@@ -1,7 +1,10 @@
 extends Resource
 class_name GameSettings
 
+## Saved graphics settings
 @export var graphics: Dictionary
+## Constant default control scheme
+var default_controls: Dictionary
 ## Saved control scheme
 @export var controls: Dictionary
 
@@ -11,11 +14,11 @@ signal request_viewport_change(change_tag: String, value: Variant)
 # when the resource finishes loading from file
 func _init():
 	graphics = {
-		"fullscreen":  true if ProjectSettings.get_setting("display/window/size/mode") == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN else false
+		"windowMode":  ProjectSettings.get_setting("display/window/size/mode")
 	}
 	
 	# Manually assigning modifiable controls
-	controls = {
+	default_controls = {
 		"move_hover": InputMap.action_get_events("move_hover"),
 		"end_turn": InputMap.action_get_events("end_turn"),
 		"use_ability_1": InputMap.action_get_events("use_ability_1"),
@@ -23,49 +26,53 @@ func _init():
 		"use_ability_3": InputMap.action_get_events("use_ability_3"),
 		"use_ability_4": InputMap.action_get_events("use_ability_4")
 	}
+	
+	controls = default_controls
+
 
 ## Apply the graphics settings that have been saved
-func apply_graphics_from_file():
+func apply_graphics_from_saved():
 	print("applying settings...")
 	for tag in graphics:
-		apply_graphics_setting(tag)
+		request_viewport_change.emit(tag, graphics[tag])
 		prints(tag, graphics[tag])
-
-## Applies the given graphics setting from the saved value
-func apply_graphics_setting(tag: String):
-	# Because each setting needs to be treated differently, we need a match tree to set every setting
-	request_viewport_change.emit(tag, graphics[tag])
 
 
 #TODO: Implement timed preview on graphics changes
-#TODO: Add an apply button instead of toggling instantly
-func toggle_graphics_setting(tag: String, val: bool):
+func toggle_graphics_setting(tag: String, val: Variant):
 	graphics[tag] = val
-	apply_graphics_setting(tag)
 
 
 ## Clear default bindings and apply saved ones
-func bind_controls_from_file():
+func bind_controls_from_saved():
 	for action in controls:
 		InputMap.action_erase_events(action)
 		for event in controls[action]:
-			InputMap.action_add_event(action, event)
+			if event != null:
+				InputMap.action_add_event(action, event)
 
 
 ## Rebind a single control
+# Supports null values. If old is null, acts as a bind. if new is null, acts as an unbind
 func rebind_control(action: String, old: InputEvent, new: InputEvent):
-	InputMap.action_erase_event(action, old)
-	InputMap.action_add_event(action, new)
-	controls[action] = InputMap.action_get_events(action)
+	var index = controls[action].find(old)
+	controls[action][index] = new
+	
+	#InputMap.action_erase_event(action, old)
+	#InputMap.action_add_event(action, new)
+	#controls[action] = InputMap.action_get_events(action)
 
-
-## Bind a single control
-func bind_control(action: String, new: InputEvent):
-	InputMap.action_add_event(action, new)
-	controls[action] = InputMap.action_get_events(action)
-
-
-## Unbind a single control
-func unbind_control(action: String, new: InputEvent):
-	InputMap.action_erase_event(action, new)
-	controls[action] = InputMap.action_get_events(action)
+#
+### Bind a single control
+#func bind_control(action: String, new: InputEvent):
+	#if null in controls[action]:
+		#
+	#
+	#InputMap.action_add_event(action, new)
+	#controls[action] = InputMap.action_get_events(action)
+#
+#
+### Unbind a single control
+#func unbind_control(action: String, new: InputEvent):
+	#InputMap.action_erase_event(action, new)
+	#controls[action] = InputMap.action_get_events(action)
